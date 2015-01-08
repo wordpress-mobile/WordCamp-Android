b728bc07d952360b37c1ec730def09ceffd04991
aagam94 <aagam94@gmail.com>
2015-01-08 17:14:31 +0530

    remove factory comments

--- app/src/main/java/org/wordcamp/MyWCFragment.java
+++ app/src/main/java/org/wordcamp/MyWCFragment.java
@@ -1,40 +1,18 @@
 package org.wordcamp;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * to handle interaction events.
- * Use the {@link MyWCFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MyWCFragment extends android.support.v4.app.Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyWCFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MyWCFragment newInstance(String param1, String param2) {
         MyWCFragment fragment = new MyWCFragment();
         Bundle args = new Bundle();
--- app/src/main/java/org/wordcamp/UpcomingWCFragment.java
+++ app/src/main/java/org/wordcamp/UpcomingWCFragment.java
@@ -1,6 +1,5 @@
 package org.wordcamp;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,34 +11,17 @@ import android.widget.TextView;
 import org.wordcamp.adapters.UpcomingWCAdapter;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * to handle interaction events.
- * Use the {@link UpcomingWCFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class UpcomingWCFragment extends android.support.v4.app.Fragment {
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
     private RecyclerView rView;
     private RecyclerView.LayoutManager mLayoutManager;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UpcomingWCFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static UpcomingWCFragment newInstance(String param1, String param2) {
         UpcomingWCFragment fragment = new UpcomingWCFragment();
         Bundle args = new Bundle();
