8ebf166d329b4c91945ff083dc0218999900b337
aagam94 <aagam94@gmail.com>
2015-02-02 02:10:21 +0530

    show info about speakers on clicking

--- app/src/main/java/org/wordcamp/wcdetails/SpeakerFragment.java
+++ app/src/main/java/org/wordcamp/wcdetails/SpeakerFragment.java
@@ -1,13 +1,19 @@
 package org.wordcamp.wcdetails;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.wordcamp.R;
 import org.wordcamp.WordCampDetailActivity;
@@ -53,10 +59,26 @@ public class SpeakerFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_wcdetails_speaker,container,false);
         lv = (ListView)v.findViewById(R.id.speaker_list);
         lv.setAdapter(adapter);
+
+
+
+
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                LayoutInflater dInflater = getActivity().getLayoutInflater();
+                View dialog = dInflater.inflate(R.layout.speaker_deatils, null);
+                TextView name = (TextView)dialog.findViewById(R.id.speaker_details_name);
+                TextView info = (TextView)dialog.findViewById(R.id.speaker_details_info);
+                ImageView dp = (ImageView)dialog.findViewById(R.id.speaker_details_dp);
+                Picasso.with(getActivity()).load(speakerDBList.get(position).getGravatar()).into(dp);
+                name.setText(speakerDBList.get(position).getName());
+                info.setText(Html.fromHtml(speakerDBList.get(position).getInfo()));
 
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setView(dialog);
+                builder.create();
+                builder.show();
             }
         });
         return v;
