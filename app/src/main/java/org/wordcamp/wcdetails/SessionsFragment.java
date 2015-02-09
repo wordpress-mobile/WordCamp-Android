b450b06948597b69e1f8fb503dd4f2b54bfa182e
aagam94 <aagam94@gmail.com>
2015-02-09 22:52:21 +0530

    use NewSessionFragment as SessionFragment

--- app/src/main/java/org/wordcamp/wcdetails/SessionsFragment.java
+++ app/src/main/java/org/wordcamp/wcdetails/SessionsFragment.java
@@ -4,17 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.wordcamp.R;
 import org.wordcamp.WordCampDetailActivity;
-import org.wordcamp.adapters.SessionsListAdapter;
+import org.wordcamp.adapters.NewSessListAdapter;
 import org.wordcamp.objects.SessionDB;
 
 import java.util.List;
+
+import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 
 /**
  * Created by aagam on 31/1/15.
@@ -22,8 +23,8 @@ import java.util.List;
 public class SessionsFragment extends Fragment {
 
     private GridLayoutManager mLayoutManager;
-    private RecyclerView mRecyclerView;
-    private SessionsListAdapter sessionsListAdapter;
+    private StickyListHeadersListView sessionList;
+    private NewSessListAdapter sessionsListAdapter;
     private List<SessionDB> sessionDBList;
     private int wcid;
     @Override
@@ -35,38 +36,19 @@ public class SessionsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fargment_sessions_list,container,false);
-        mLayoutManager = new GridLayoutManager(getActivity(), 2);
-        mRecyclerView = (RecyclerView)v.findViewById(R.id.recyclerView);
-
-      /*  StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
-        layoutManager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS);*/
-        mLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
-            @Override
-            public int getSpanSize(int position) {
-               if(sessionDBList.get(position).getTitle().equals("Keynote")
-                       ||sessionDBList.get(position).getTitle().equals("Lunch")){
-                   return 2;
-               }
-                return 1;
-            }
-        });
-
-
-
-
-        mRecyclerView.setLayoutManager(mLayoutManager);
+        View v = inflater.inflate(R.layout.fargment_sessions_list_new,container,false);
+        sessionList = (StickyListHeadersListView)v.findViewById(R.id.sessionList);
         if(sessionDBList!=null)
-            sessionsListAdapter = new SessionsListAdapter(sessionDBList);
-        mRecyclerView.setAdapter(sessionsListAdapter);
+            sessionsListAdapter = new NewSessListAdapter(getActivity(),sessionDBList);
+
+
+        sessionList.setAdapter(sessionsListAdapter);
         return v;
     }
 
     public void updateData(){
-        int position = ((GridLayoutManager) mRecyclerView.getLayoutManager()).findFirstCompletelyVisibleItemPosition();
         sessionDBList = ((WordCampDetailActivity)getActivity()).communicator.getAllSession(wcid);
-        sessionsListAdapter = new SessionsListAdapter(sessionDBList);
-        mRecyclerView.setAdapter(sessionsListAdapter);
-        mRecyclerView.scrollToPosition(position);
+        sessionsListAdapter = new NewSessListAdapter(getActivity(),sessionDBList);
+        sessionList.setAdapter(sessionsListAdapter);
     }
 }
