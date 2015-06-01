be3ec75e8cfacc0240983c472b253a82b86c76b3
aagam94 <aagam94@gmail.com>
2015-06-02 01:45:44 +0530

    send mail with logs from FeedbackActivity

--- app/src/main/java/org/wordcamp/FeedbackActivity.java
+++ app/src/main/java/org/wordcamp/FeedbackActivity.java
@@ -1,33 +1,25 @@
 package org.wordcamp;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.gson.Gson;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
+import org.wordcamp.utils.Utils;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by aagam on 27/5/15.
@@ -36,6 +28,7 @@ public class FeedbackActivity extends AppCompatActivity {
 
     private EditText title, descr;
     private SwitchCompat bugSwitch;
+    private String LOG_DIR_NAME;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,7 +77,7 @@ public class FeedbackActivity extends AppCompatActivity {
         protected void onPreExecute() {
             super.onPreExecute();
             dialog = new ProgressDialog(FeedbackActivity.this);
-            dialog.setMessage("Posting...");
+            dialog.setMessage("Preparing...");
             dialog.setCancelable(false);
             dialog.setCanceledOnTouchOutside(false);
             dialog.setIndeterminate(true);
@@ -94,58 +87,24 @@ public class FeedbackActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... params) {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpPost httppost = new HttpPost(BuildConfig.FEEDBACK_URL);
-            String logs = "";
-            try {
+            if (bugSwitch.isChecked()) {
+                try {
+                    Process process = Runtime.getRuntime().exec("logcat -d");
+                    BufferedReader bufferedReader = new BufferedReader(
+                            new InputStreamReader(process.getInputStream()));
 
-                List<NameValuePair> nameValuePairs = new ArrayList<>();
-                nameValuePairs.add(new BasicNameValuePair("title", title.getText().toString()));
-                nameValuePairs.add(new BasicNameValuePair("descr", descr.getText().toString()));
-                if (bugSwitch.isChecked()) {
-                    nameValuePairs.add(new BasicNameValuePair("cat", "issue"));
-                    try {
-                        Process process = Runtime.getRuntime().exec("logcat -d");
-                        BufferedReader bufferedReader = new BufferedReader(
-                                new InputStreamReader(process.getInputStream()));
-
-                        StringBuilder log = new StringBuilder();
-                        String line;
-                        while ((line = bufferedReader.readLine()) != null) {
-                            log.append(line);
-                        }
-                        logs = log.toString();
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    StringBuilder log = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        log.append(line);
                     }
-                } else {
-                    nameValuePairs.add(new BasicNameValuePair("cat", "enhancement"));
+                    String logs = log.toString();
+                    return logs;
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-
-                String device = Build.VERSION.SDK_INT + " "         // API Level
-                        + Build.DEVICE + " "                     // Device
-                        + Build.MANUFACTURER + " "                       // Manufacturer
-                        + Build.MODEL + " "                       // Model
-                        + Build.PRODUCT;
-                nameValuePairs.add(new BasicNameValuePair("device", device));
-
-                Gson gson = new Gson();
-                String json = gson.toJson(logs);
-                nameValuePairs.add(new BasicNameValuePair("logs", json));
-                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-                HttpResponse response = httpclient.execute(httppost);
-                if (response != null) {
-                    return response.toString();
-                }
-
-            } catch (ClientProtocolException e) {
-                e.printStackTrace();
-                return null;
-            } catch (IOException e) {
-                e.printStackTrace();
-                return null;
             }
+
             return null;
         }
 
@@ -154,12 +113,59 @@ public class FeedbackActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             dialog.dismiss();
-            if (s == null) {
-                Toast.makeText(FeedbackActivity.this, "Can't connect to server", Toast.LENGTH_SHORT).show();
+
+            String device = Utils.getDeviceInfo();
+
+            if (s != null) {
+                LOG_DIR_NAME = getFilePath();
+                if (LOG_DIR_NAME != null) {
+                    try {
+                        File f = new File(LOG_DIR_NAME, "applog.txt");
+                        if (f.exists()) {
+                            f.delete();
+                        }
+
+                        FileOutputStream fos = new FileOutputStream(f);
+                        fos.write(s.getBytes());
+                        fos.close();
+
+                        Uri u = Uri.fromFile(f);
+                        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                        emailIntent.setType("plain/text");
+                        emailIntent.putExtra(Intent.EXTRA_EMAIL,
+                                new String[]{BuildConfig.FEEDBACK_MAIL});
+                        emailIntent.putExtra(Intent.EXTRA_STREAM, u);
+                        emailIntent.putExtra(Intent.EXTRA_SUBJECT, title.getText().toString());
+                        emailIntent.putExtra(Intent.EXTRA_TEXT, descr.getText().toString() + device);
+                        startActivity(emailIntent);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             } else {
-                Toast.makeText(FeedbackActivity.this, "Successfully posted", Toast.LENGTH_SHORT).show();
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL,
+                        new String[]{BuildConfig.FEEDBACK_MAIL});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, title.getText().toString());
+                emailIntent.putExtra(Intent.EXTRA_TEXT, descr.getText().toString() + device);
+                startActivity(emailIntent);
             }
         }
 
     }
+
+    public String getFilePath() {
+
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            String s = Environment.getExternalStorageDirectory().toString() + "/wc";
+            return s;
+        } else {
+            return null;
+        }
+    }
+
+
 }
--- app/src/main/java/org/wordcamp/utils/Utils.java
+++ app/src/main/java/org/wordcamp/utils/Utils.java
@@ -5,9 +5,18 @@ import android.os.Build;
 /**
  * Created by aagam on 3/3/15.
  */
-class Utils {
+public class Utils {
 
-    public static boolean isLollipop(){
-        return Build.VERSION.SDK_INT>=21;
+    public static boolean isLollipop() {
+        return Build.VERSION.SDK_INT >= 21;
+    }
+
+    public static String getDeviceInfo() {
+        return "\n\n Device info :- \n"
+                + "API : " + Build.VERSION.SDK_INT + "\n "              // API Level
+                + "Device : " + Build.DEVICE + "\n"                     // Device
+                + "Manufacturer : " + Build.MANUFACTURER + "\n"         // Manufacturer
+                + "Model : " + Build.MODEL + "\n"                       // Model
+                + "Product : " + Build.PRODUCT;
     }
 }
